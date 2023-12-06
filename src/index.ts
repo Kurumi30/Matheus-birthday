5cc83800d3632f3edaa7bf5345d72a5d5b7373c9
Kurumi30 <bsd3su@gmail.com>
2023-12-05 22:05:27 -0300

    Update video links and add default URL

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
 import { links } from '../public/json/links.json'
 
+const defaultUrl: string = "https://cdn.mtdv.me/video"
+
 export function setupCounter(element: HTMLButtonElement): void {
   let counter = 0
+
   const setCounter = (count: number): void => {
     counter = count
     element.innerHTML = `count is ${counter}`
   }
+
   element.addEventListener('click', () => {
     setCounter(counter + 1)
 
     if (counter >= 10) {
-      location.href = links[Math.floor(Math.random() * links.length)]
+      const link = defaultUrl + links[Math.floor(Math.random() * links.length)]
+
+      location.href = link
 
       element.innerHTML = "Preparando a última surpresa..."
     }
   })
+
   setCounter(0)
 }
