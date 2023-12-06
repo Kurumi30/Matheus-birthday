b2ab72f835234aa77014f36ce66803761f9da1db
Kurumi30 <bsd3su@gmail.com>
2023-12-05 22:06:03 -0300

    Remove commented out code

--- src/js/glitch.js
+++ src/js/glitch.js
@@ -7,36 +7,6 @@ const createNotification = () => {
   //   alert("Este navegador não suporta notificações de sistema")
 
   //   return
-  // } else if (Notification.permission === "granted") {
-  //   // Se a permissão já foi concedida
-  //   new Notification("Olá! Esta é uma notificação.", {
-  //     body: "Este é o corpo da notificação",
-  //     icon: "../../public/svg/vite.svg",
-  //     vibrate: [200, 100, 200, 100, 200, 100, 200],
-  //   })
-
-  //   return
-  // } else if (Notification.permission !== "denied") {
-  //   // Caso contrário, precisamos perguntar ao usuário para permissão
-  //   Notification
-  //     .requestPermission()
-  //     .then(permission => {
-  //       // Se o usuário aceitar, vamos criar uma notificação
-  //       if (permission === "granted") {
-  //         new Notification("Olá! Esta é uma notificação.", {
-  //           body: "Mensagem para trazer o amiguinho de volta",
-  //           icon: "../../public/svg/vite.svg",
-  //           vibrate: [200, 100, 200, 100, 200, 100, 200],
-  //         })
-  //       }
-
-  //       return
-  //     })
-  //     .catch(err => {
-  //       console.error(err)
-  //     })
-
-  //   return
   // }
 
   if (Notification && Notification.permission !== "denied") {
@@ -65,6 +35,7 @@ document.addEventListener('visibilitychange', () => {
   // if (document.visibilityState == 'hidden') title = "Volte aqui!"
   if (document.hidden) {
     document.title = "Volte aqui!"
+    
     createNotification()
   } else {
     document.title = title
