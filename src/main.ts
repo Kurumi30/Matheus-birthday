ec4c3a92662df9c3ace15f3d3df4e1e69ae92d7f
Kurumi30 <bsd3su@gmail.com>
2023-12-10 22:04:50 -0300

    Update main.ts with new content

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,15 @@
 import './css/page.css'
-import typescriptLogo from '/svg/typescript.svg'
 import { setupCounter } from '.'
+// import typescriptLogo from '/svg/typescript.svg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/svg/vite.svg" class="logo" alt="Vite logo"/>
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo"/>
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>E chegamos até o fim😪</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Do seu primeiro e eterno "<b>fã</b>", Fernando <span class="heart">❤</span>
     </p>
   </div>
 `
